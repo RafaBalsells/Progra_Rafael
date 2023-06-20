a57829bfc26db9549c0869aff4be8996d62967c4
RafaBalsells <136516721+RafaBalsells@users.noreply.github.com>
2023-06-20 16:09:26 -0600

    Add files via upload

---
+++ Progra Lunes 19-06 Rafael.cpp
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    char nombre[5];
+
+    for(int i = 0; i < 5; i++){
+        cout <<"nombre: ";
+        cin.getline(nombre, 5, '\n');
+
+        cout <<nombre <<endl;
+
+        cout <<strlen(nombre) <<endl <<endl;
+    }
+}
+
